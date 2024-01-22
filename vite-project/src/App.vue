23ef73571948ecbded45df3557152dff65cb1092
Alberto Miron <147499328+MironAlberto@users.noreply.github.com>
2024-01-22 16:14:28 +0100

    Initial commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import Axios from 'axios';
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },  
+    methods: {
+
+    },
+    mounted(){
+        Axios.get.apply().then((response) => {
+            console.log(response);
+        });
+    }
+}
+</script>
+
+<template>
+    <h1>
+        Template Vite 
+    </h1>
+
+    <AppHeader/>
+
+    <AppMain/>
+
+    <AppFooter/>
+</template>
+
+<style lang="scss">
+@use "assets/scss/main" as *;
+</style>
